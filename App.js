9a3788d876ceba44df66709524ffdd19eab3acc7
TIP-QC <qjbaocampo@tip.edu.ph>
2023-10-26 09:33:04 +0800

    UPDATED messaging_app

--- App.js
+++ App.js
@@ -1,36 +1,46 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import StatusBarComponent from './components/status_bar'; 
+import { Constants } from "expo";
+
+import {
+  NetInfo,
+  Platform,
+  StatusBar,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
+import React from "react";
+import Status from "./components/status_bar";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <StatusBarComponent />
+      <Text>
+        Mikaela Faye E. Popes
+        CPE41S1
+      </Text>
+      <Status />
     </View>
   );
 }
 
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'white',
-    // alignItems: 'center',
-    // justifyContent: 'center',
+    backgroundColor: "white",
+    alignItems: "center",
+    justifyContent: "center",
   },
   content: {
     flex: 1,
-    backgroundColor: 'white',
- },
- inputMethodEditor: {
-  flex: 1,
-  backgroundColor: 'white',
- },
- toolbar: {
-  borderTopWidth: 1, 
-  borderTopColor: 'rgba(0, 0, 0, 0.04)',
-  backgroundColor: 'white',
- }
-
-
+    backgroundColor: "white",
+  },
+  inputMethodEditor: {
+    flex: 1,
+    backgroundColor: "white",
+  },
+  toolbar: {
+    borderTopWidth: 1,
+    borderTopColor: "rgba(0,0,0,0.04)",
+    backgroundColor: "white",
+  },
 });
--- components/status_bar.js
+++ components/status_bar.js
@@ -1,55 +1,92 @@
-import Constants from "expo-constants";
-import { StatusBar, StyleSheet, Text, View } from "react-native";
-import NetInfo from '@react-native-community/netinfo';
-import React, { Component } from "react";
+import { Constants } from "expo";
+import { Platform, StatusBar, StyleSheet, Text, View, Animated } from "react-native";
+import React, { Component,useRef, useEffect } from "react";
 
-export default class StatusB extends Component {
-    state = {
-        info: null
-      };
-   
-      render() {
-         // const {info} = this.state;
-         const isConnected = NetInfo.addEventListener(state => {
-            state.isConnected ? true : false;
-         });
-         // const isConnected = info !== 'none';
-         const backgroundColor = isConnected ? 'white': 'red';
-         // Subscribec
-         const statusBar = <StatusBar
-            backgroundColor={backgroundColor}
-            barStyle={isConnected ? 'dark-content' : 'light-content'}
-            animated={false}
-            />
-         const messageContainer = (
-            <View style={styles.messageContainer} pointerEvents={'none'}>
-               {statusBar}
-               {!isConnected && (
-                  <View style={styles.bubble}>
-                     <Text style={styles.text}>No Network Connection</Text>
-                     {console.log('working')}
-                  </View>
-               )}
-            </View>
-         )
-         if(Platform.OS == 'ios') {
-            return (
-            <View style={[styles.status, backgroundColor]}>
-               {messageContainer}
-            </View>
-            )
-         }
-         return messageContainer; 
-      }
-   }
-   
-   let statusHeight = (Platform.OS == 'ios' ? statusHeight : 0);
+const FadeInView = props => {
+  const fadeAnim = useRef(new Animated.Value(0)).current; // Initial value for opacity: 0
+
+  useEffect(() => {
+    Animated.timing(fadeAnim, {
+      toValue: 1,
+      duration: 2000,
+      useNativeDriver: true,
+    }).start();
+  }, [fadeAnim]);
+
+  return (
+    <Animated.View // Special animatable View
+      style={{
+        ...props.style,
+        opacity: fadeAnim, // Bind opacity to animated value
+      }}>
+      {props.children}
+    </Animated.View>
+  );
+};
+
+export default class Status extends Component {
+  state = {
+    info: null,
+    isConnected: true,
+  };
+
+  componentDidMount() {
+    // Here you can use the NetInfo library to listen to network connectivity changes.
+    // Make sure to install the '@react-native-community/netinfo' library and import it.
+
+    // For this example, we'll simulate the network status change after a delay.
+    setTimeout(() => {
+      this.handleConnectivityChange(false); // Simulate a disconnected state
+    }, 5000);
+  }
+
+  handleConnectivityChange = (isConnected) => {
+    this.setState({ isConnected });
+  };
+
+  render() {
+    const { isConnected } = this.state;
+    const backgroundColor = isConnected ? "cyan" : "red";
+    const statusBar = (
+      <StatusBar
+        backgroundColor={backgroundColor}
+        barStyle={isConnected ? "dark-content" : "light-content"}
+        animated={false}
+      />
+    );
+
+    const messageContainer = (
+      <View style={styles.messageContainer}>
+        {statusBar}
+        {!isConnected && (
+          <View style={styles.bubble}>
+            <FadeInView>
+              <Text style={styles.text}>No network connection</Text>
+            </FadeInView>
+          </View>
+        )}
+      </View>
+    );
+
+    if (Platform.OS === "ios") {
+      return (
+        <View style={[styles.status, { backgroundColor }]}>
+          {messageContainer}
+        </View>
+      );
+    }
+
+    return messageContainer;
+  }
+}
+
+const statusHeight = Platform.OS == "ios" ? statusHeight : 0;
 
 const styles = StyleSheet.create({
   messageContainer: {
     zIndex: 1,
     position: "absolute",
-    top: Constants.statusBarHeight + 20,
+    top: statusHeight + 20,
     right: 0,
     left: 0,
     height: 80,
@@ -59,9 +96,9 @@ const styles = StyleSheet.create({
     paddingHorizontal: 20,
     paddingVertical: 10,
     borderRadius: 20,
-    backgroundColor: 'red',
+    backgroundColor: "red",
   },
   text: {
-    color: 'white',
+    color: "white",
   },
 });
